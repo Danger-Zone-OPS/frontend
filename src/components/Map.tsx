0fb5cef0a3d0e0b67352a70ebd737a43e73ff1ed
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-10-21 11:56:39 +0200

    Nice map theme

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -67,8 +67,8 @@ export function Map(props: MapProps) {
       )}
 
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
 
       {newAreaPoints.length > 0 && (
